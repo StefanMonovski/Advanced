6b671a993f4301adb75340527ae2566fe7772c8b
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 15:57:55 +0300

    Add ReverseStrings

---
+++ 01.StacksAndQueues/01.ReverseStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> input = new Stack<char>(Console.ReadLine());
+            string output = "";
+            while (input.Count != 0)
+            {
+                output += input.Pop();
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
