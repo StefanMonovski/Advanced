6c4f1675d3421484a313b21aed78c87f1e47a44b
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 18:13:06 +0300

    Add MaximumAndMinimumElement

---
+++ 02.StacksAndQueues-Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberQueries = int.Parse(Console.ReadLine());
+            Stack<int> numbers = new Stack<int>();
+            for (int i = 0; i < numberQueries; i++)
+            {
+                List<string> input = Console.ReadLine().Split().ToList();
+                int query = int.Parse(input[0]);
+                if (query == 1)
+                {
+                    int number = int.Parse(input[1]);
+                    numbers.Push(number);
+                }
+                else if (query == 2)
+                {
+                    numbers.Pop();
+                }
+                else if (query == 3 && numbers.Count != 0)
+                {
+                    int[] numbersArray = numbers.ToArray();
+                    Console.WriteLine(numbersArray.Max());
+                }
+                else if (query == 4 && numbers.Count != 0)
+                {
+                    int[] numbersArray = numbers.ToArray();
+                    Console.WriteLine(numbersArray.Min());
+                }
+            }
+            int[] numbersOutput = numbers.ToArray();
+            Console.WriteLine(string.Join(", ", numbersOutput));
+        }
+    }
+}
