2b0cafb8d772763f3e2664095489d020df25b979
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 17:01:10 +0300

    Add HotPotato

---
+++ 01.StacksAndQueues/07.HotPotato/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> children = new Queue<string>(Console.ReadLine().Split());
+            int tosses = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (children.Count != 1)
+            {
+                string currentChild = children.Dequeue();
+                if (counter == tosses)
+                {
+                    Console.WriteLine($"Removed {currentChild}");
+                    counter = 0;
+                }
+                else
+                {
+                    children.Enqueue(currentChild);
+                }
+                counter++;
+            }
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
