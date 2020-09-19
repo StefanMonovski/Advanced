829b0fea6cc416caa04caa8d7d2e4110ce02b79f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 16:35:03 +0300

    Add PrintEvenNumbers

---
+++ 01.StacksAndQueues/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+            Queue<int> numbers = new Queue<int>(input.Select(int.Parse));
+            int totalNumbers = numbers.Count;
+            for (int i = 0; i < totalNumbers; i++)
+            {
+                int number = numbers.Dequeue();
+                if (number % 2 == 0)
+                {
+                    numbers.Enqueue(number);
+                }
+            }
+            int[] evenNumbers = numbers.ToArray();
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
