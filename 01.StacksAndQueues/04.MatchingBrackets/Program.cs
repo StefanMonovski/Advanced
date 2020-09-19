8f177229cc52f27163b14d61fb84b78ddbd450b9
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 16:14:49 +0300

    Add MatchingBrackets

---
+++ 01.StacksAndQueues/04.MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> indexes = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int startIndex = 0;
+                int endIndex = 0;
+                if (input[i] == '(')
+                {
+                    indexes.Push(i);
+                }
+                if (input[i] == ')')
+                {
+                    startIndex = indexes.Pop();
+                    endIndex = i;
+                    string substring = input.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(substring);
+                }
+            }
+        }
+    }
+}
