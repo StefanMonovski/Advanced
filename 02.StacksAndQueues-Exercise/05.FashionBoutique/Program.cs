02501ae218e292d988bd4b4804ee4a3d7727bb72
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 19:28:28 +0300

    Add FashionBoutique

---
+++ 02.StacksAndQueues-Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+            Stack<int> clothesValues = new Stack<int>(input.Select(int.Parse));
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int currentRack = 0;
+            int totalRacks = 0;
+            while (clothesValues.Count != 0)
+            {
+                int currentValue = clothesValues.Pop();
+                if (rackCapacity > currentRack + currentValue)
+                {
+                    currentRack += currentValue;
+                }
+                else if (rackCapacity == currentRack + currentValue)
+                {
+                    currentRack = 0;
+                    totalRacks++;
+                }
+                else if (rackCapacity < currentRack + currentValue)
+                {
+                    currentRack = currentValue;
+                    totalRacks++;
+                }
+            }
+            if (currentRack != 0)
+            {
+                totalRacks++;
+            }
+            Console.WriteLine(totalRacks);
+        }
+    }
+}
