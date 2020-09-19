ba8b683859f830bd76b00348a6a924504e756af5
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 16:46:25 +0300

    Add Supermarket

---
+++ 01.StacksAndQueues/06.Supermarket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> clients = new Queue<string>();
+            string input = "";
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (clients.Count != 0)
+                    {
+                        Console.WriteLine(clients.Dequeue());
+                    }
+                }
+                else
+                {
+                    clients.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{clients.Count} people remaining.");
+        }
+    }
+}
