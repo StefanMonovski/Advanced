598e18fdc762cd4cafa2205bf93abe86070ba5a2
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 18:57:32 +0300

    Add FastFood

---
+++ 02.StacksAndQueues-Exercise/04.FastFood/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityFood = int.Parse(Console.ReadLine());
+            List<string> input = Console.ReadLine().Split().ToList();
+            Queue<int> orders = new Queue<int>(input.Select(int.Parse));
+            int[] ordersArray = orders.ToArray();
+            Console.WriteLine(ordersArray.Max());
+            int totalOrders = orders.Count;
+            for (int i = 0; i < totalOrders; i++)
+            {
+                int currentOrder = orders.Peek();
+                if (quantityFood >= currentOrder)
+                {
+                    quantityFood -= currentOrder;
+                    orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (orders.Count != 0)
+            {
+                Console.Write("Orders left: ");
+                while (orders.Count != 0)
+                {
+                    Console.Write($"{orders.Dequeue()} ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
