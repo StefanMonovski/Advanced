613dfb9294718dcf4a1b16fe2e411296308b2452
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-19 17:17:54 +0300

    Add TrafficJam

---
+++ 01.StacksAndQueues/08.TrafficJam/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCarsPassGreen = int.Parse(Console.ReadLine());
+            int totalCarsPassGreen = 0;
+            Queue<string> cars = new Queue<string>();
+            string input = "";
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    int counter = 0;
+                    while (counter < numberCarsPassGreen && cars.Count != 0)
+                    {
+                        string car = cars.Dequeue();
+                        Console.WriteLine($"{car} passed!");
+                        totalCarsPassGreen++;
+                        counter++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{totalCarsPassGreen} cars passed the crossroads.");
+        }
+    }
+}
