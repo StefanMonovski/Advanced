a77964a62eac8db48e16178d8cca34b1e71c12e6
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-20 14:51:09 +0300

    Add Crossroads

---
+++ 02.StacksAndQueues-Exercise/10.Crossroads/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLightSeconds = int.Parse(Console.ReadLine());
+            int freeWindowSeconds = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            bool crashHappened = false;
+            int carsPassedSafely = 0;
+            string input = "";
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input != "green")
+                {
+                    cars.Enqueue(input);
+                }
+                else
+                {
+                    string currentCar = "";
+                    if (cars.Count > 0)
+                    {
+                        currentCar = cars.Dequeue();
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    int currentGreenLightSeconds = greenLightSeconds;
+                    while (currentGreenLightSeconds > 0)
+                    {
+                        if (currentCar.Length < currentGreenLightSeconds)
+                        {
+                            currentGreenLightSeconds -= currentCar.Length;
+                            carsPassedSafely++;
+                            if (cars.Count > 0)
+                            {
+                                currentCar = cars.Dequeue();
+                                continue;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        else if (currentCar.Length >= currentGreenLightSeconds && currentCar.Length <= currentGreenLightSeconds + freeWindowSeconds)
+                        {
+                            currentGreenLightSeconds -= currentCar.Length;
+                            carsPassedSafely++;
+                        }
+                        else if (currentCar.Length > currentGreenLightSeconds + freeWindowSeconds)
+                        {
+                            crashHappened = true;
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{currentCar} was hit at {currentCar[currentGreenLightSeconds + freeWindowSeconds]}.");
+                            break;
+                        }
+                    }
+                    if (crashHappened)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (!crashHappened)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{carsPassedSafely} total cars passed the crossroads.");
+            }
+        }
+    }
+}
