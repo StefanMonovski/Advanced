dcbf992e67e9e6fefb36af070e2b7d80ad265cda
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-24 22:13:05 +0300

    Add PrimaryDiagonal

---
+++ 03.MultidimensionalArrays/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] intMatrix = new int[matrixSize, matrixSize];
+            for (int i = 0; i < intMatrix.GetLength(0); i++)
+            {
+                int[] intColumns = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < intMatrix.GetLength(1); j++)
+                {
+                    intMatrix[i, j] = intColumns[j];
+                }
+            }
+            int sumPrimaryDiagonal = 0;
+            for (int i = 0; i < matrixSize; i++)
+            {
+                sumPrimaryDiagonal += intMatrix[i, i];
+            }
+            Console.WriteLine(sumPrimaryDiagonal);
+        }
+    }
+}
