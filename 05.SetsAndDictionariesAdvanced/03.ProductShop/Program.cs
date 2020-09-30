ced88191be72c3fd10f3cc4483d1c7a913ed8998
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-30 23:45:35 +0300

    Add ProductShop

---
+++ 05.SetsAndDictionariesAdvanced/03.ProductShop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shopsData = new Dictionary<string, Dictionary<string, double>>();
+            string input = "";
+            while ((input = Console.ReadLine()) != "Revision")
+            {
+                List<string> inputData = input.Split(", ").ToList();
+                string shop = inputData[0];
+                string product = inputData[1];
+                double price = double.Parse(inputData[2]);
+                if (shopsData.ContainsKey(shop))
+                {
+                    shopsData[shop].Add(product, price);
+                }
+                else
+                {
+                    shopsData.Add(shop, new Dictionary<string, double>());
+                    shopsData[shop].Add(product, price);
+                }
+            }
+            var shopsDataOrdered = shopsData.OrderBy(x => x.Key);
+            foreach (var item in shopsDataOrdered)
+            {
+                Console.WriteLine($"{item.Key}->");
+                foreach (var product in item.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
