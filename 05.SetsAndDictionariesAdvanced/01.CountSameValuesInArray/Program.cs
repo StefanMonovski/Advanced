2e2b9c922dafa504165c36be11440a5bd0e43163
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-30 22:59:22 +0300

    Add CountSameValuesInArray

---
+++ 05.SetsAndDictionariesAdvanced/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputArray = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> countSameValues = new Dictionary<double, int>();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (countSameValues.ContainsKey(inputArray[i]))
+                {
+                    countSameValues[inputArray[i]]++;
+                }
+                else
+                {
+                    countSameValues.Add(inputArray[i], 1);
+                }
+            }
+            foreach (var item in countSameValues)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
