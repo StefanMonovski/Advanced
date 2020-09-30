b79622b4ccb39b7f597d1512ed39ffa0550553c2
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-30 23:29:40 +0300

    Add AverageStudentGrades

---
+++ 05.SetsAndDictionariesAdvanced/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalStudents = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> studentGrades = new Dictionary<string, List<decimal>>();
+            for (int i = 0; i < totalStudents; i++)
+            {
+                List<string> currentStudentData = Console.ReadLine().Split().ToList();
+                string currentStudent = currentStudentData[0];
+                decimal currentGrade = decimal.Parse(currentStudentData[1]);
+                if (studentGrades.ContainsKey(currentStudentData[0]))
+                {
+                    studentGrades[currentStudent].Add(currentGrade);
+                }
+                else
+                {
+                    studentGrades.Add(currentStudent, new List<decimal>());
+                    studentGrades[currentStudent].Add(currentGrade);
+                }
+            }
+            foreach (var item in studentGrades)
+            {
+                decimal currentStudentAverage = studentGrades[item.Key].Average();
+                Console.Write($"{item.Key} -> ");
+                foreach (decimal grade in studentGrades[item.Key])
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.WriteLine($"(avg: {currentStudentAverage:f2})");
+            }
+        }
+    }
+}
