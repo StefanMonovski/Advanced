5daa39c240d07bb3bad230537cb4c450382b411f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-01 19:38:06 +0300

    Add ParkingLot

---
+++ 05.SetsAndDictionariesAdvanced/06.ParkingLot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            HashSet<string> carsParking = new HashSet<string>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                List<string> inputData = input.Split(", ").ToList();
+                string direction = inputData[0];
+                string carNumber = inputData[1];
+                if (direction == "IN")
+                {
+                    carsParking.Add(carNumber);
+                }
+                else if (direction == "OUT")
+                {
+                    carsParking.Remove(carNumber);
+                }
+            }
+            if (carsParking.Count != 0)
+            {
+                foreach (string item in carsParking)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
