2bcca80fcc21c6d1486fe712050de085cbd64387
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-01 20:08:58 +0300

    Add UniqueUsernames

---
+++ 06.SetsAndDictionariesAdvanced-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            HashSet<string> uniqueUsernames = new HashSet<string>();
+            for (int i = 0; i < numberInput; i++)
+            {
+                string input = Console.ReadLine();
+                uniqueUsernames.Add(input);
+            }
+            foreach (string item in uniqueUsernames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
