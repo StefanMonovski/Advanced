d9a2f4c4f3a37258daeb8e714a5a01c8fea8721c
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-01 19:49:26 +0300

    Add SoftUniParty

---
+++ 05.SetsAndDictionariesAdvanced/07.SoftUniParty/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                if (Char.IsDigit(input[0]))
+                {
+                    vipGuests.Add(input);
+                }
+                else if (!Char.IsDigit(input[0]))
+                {
+                    regularGuests.Add(input);
+                }
+            }
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (vipGuests.Contains(input))
+                {
+                    vipGuests.Remove(input);
+                }
+                else if (regularGuests.Contains(input))
+                {
+                    regularGuests.Remove(input);
+                }
+            }
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+            foreach (string item in vipGuests)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (string item in regularGuests)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
