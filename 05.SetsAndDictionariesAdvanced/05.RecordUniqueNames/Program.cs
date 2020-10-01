2fee5b03c2d794246a616c7c5fe173063a28003f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-01 19:29:34 +0300

    Add RecordUniqueNames

---
+++ 05.SetsAndDictionariesAdvanced/05.RecordUniqueNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            HashSet<string> uniqueNames = new HashSet<string>();
+            for (int i = 0; i < numberInput; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+            foreach (string item in uniqueNames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
