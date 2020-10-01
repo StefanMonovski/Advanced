ac5f112608f23706af4f3dbe59e1169f875a6a2f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-01 20:25:53 +0300

    Add PeriodicTable

---
+++ 06.SetsAndDictionariesAdvanced-Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            SortedSet<string> uniqueElements = new SortedSet<string>();
+            for (int i = 0; i < numberInput; i++)
+            {
+                List<string> inputData = Console.ReadLine().Split().ToList();
+                foreach (string item in inputData)
+                {
+                    uniqueElements.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(" ", uniqueElements));
+        }
+    }
+}
