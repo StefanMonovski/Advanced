9d53a7b7a3acc36fb0af823a2713f577a629c5fa
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-03 21:35:06 +0300

    Add CountSymbols

---
+++ 06.SetsAndDictionariesAdvanced-Exercise/05.CountSymbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] textChars = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> countChars = new Dictionary<char, int>();
+            for (int i = 0; i < textChars.Length; i++)
+            {
+                if (countChars.ContainsKey(textChars[i]))
+                {
+                    countChars[textChars[i]]++;
+                }
+                else
+                {
+                    countChars.Add(textChars[i], 1);
+                }
+            }
+            var orderedCountChars = countChars.OrderBy(x => x.Key);
+            foreach (var item in orderedCountChars)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
