b034487dcce6fe0dc0de4fff2e270f4fd2f4f993
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-06 22:14:15 +0300

    Add OddLines

---
+++ 07.Streams,FilesAndDirectories/01.OddLines/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                int counter = 0;
+                string line = "";
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
