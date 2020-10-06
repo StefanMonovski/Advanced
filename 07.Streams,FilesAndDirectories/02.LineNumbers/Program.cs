7d11d4e8015bdf83c9eb8dd6a7f54199e28620c5
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-06 22:21:17 +0300

    Add LineNumbers

---
+++ 07.Streams,FilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                int counter = 1;
+                string line = "";
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
