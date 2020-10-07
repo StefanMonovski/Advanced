d6509c347f63a27f27055f26b3c39a399ba23919
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-07 21:51:14 +0300

    Add FolderSize

---
+++ 07.Streams,FilesAndDirectories/06.FolderSize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace _06.FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filesInFolder = Directory.GetFiles("../../../TestFolder");
+            double bytes = 0;
+            foreach (string item in filesInFolder)
+            {
+                FileInfo fileInfo = new FileInfo(item);
+                bytes += fileInfo.Length;
+            }
+            double kilobytes = bytes / 1024;
+            double megabytes = kilobytes / 1024;
+            File.WriteAllText("../../../Output.txt", megabytes.ToString());
+        }
+    }
+}
