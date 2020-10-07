03f7ea62b421138b71cf1b24c54f7ad1325f872e
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-07 13:48:46 +0300

    Add SliceFile

---
+++ 07.Streams,FilesAndDirectories/05.SliceFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _05.SliceFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int filesCount = 4;
+            using (var streamReadFile = new FileStream("../../../Input.txt", FileMode.Open))
+            {
+                long fileSize = streamReadFile.Length / filesCount;
+                for (int i = 0; i < filesCount; i++)
+                { 
+                    using (var streamCreateFile = new FileStream($"../../../Part-{i + 1}.txt", FileMode.Create))
+                    {
+                        long currentFileSize = 0;
+                        byte[] buffer = new byte[1];
+                        while (currentFileSize < fileSize)
+                        {
+                            streamReadFile.Read(buffer, 0, buffer.Length);
+                            streamCreateFile.Write(buffer, 0, buffer.Length);
+                            currentFileSize += buffer.Length;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
