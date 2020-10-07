fe578e33bf755c986915386ec70be4aca2f5696c
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-07 22:32:37 +0300

    Add EvenLines

---
+++ 08.Streams,FilesAndDirectories-Exercise/01.EvenLines/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                int counter = 0;
+                string line = "";
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (counter % 2 == 0)
+                        {
+                            string pattern = @"[-,.!?]";
+                            line = Regex.Replace(line, pattern, "@");
+                            string[] lineAsArray = line.Split();
+                            Array.Reverse(lineAsArray);
+                            line = string.Join(" ", lineAsArray);
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
