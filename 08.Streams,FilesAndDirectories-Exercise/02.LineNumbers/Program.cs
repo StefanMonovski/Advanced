ac1c9c4a68f78751a94b5e456a190c0cbbd61cf5
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-07 22:56:18 +0300

    Add LineNumbers

---
+++ 08.Streams,FilesAndDirectories-Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../Input.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                int countLetters = 0;
+                int countPunctuations = 0;
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (Char.IsLetter(line[j]))
+                    {
+                        countLetters++;
+                    }
+                    else if (Char.IsPunctuation(line[j]))
+                    {
+                        countPunctuations++;
+                    }
+                }
+                lines[i] = $"Line {i + 1}:{line} ({countLetters})({countPunctuations})";
+            }
+            File.WriteAllLines("../../../Output.txt", lines);
+        }
+    }
+}
