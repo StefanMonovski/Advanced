e8552d80bd8f9e8e243010c9d56e8280f8e3eab3
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-08 22:53:10 +0300

    Add DirectoryTraversal

---
+++ 08.Streams,FilesAndDirectories-Exercise/05.DirectoryTraversal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05.DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filesInFolder = Directory.GetFiles("../../../TestFolder");
+            Dictionary<string, Dictionary<string, double>> filesInfo = new Dictionary<string, Dictionary<string, double>>();
+            foreach (var item in filesInFolder)
+            {
+                FileInfo currentFileInfo = new FileInfo(item);
+                if (filesInfo.ContainsKey(currentFileInfo.Extension))
+                {
+                    filesInfo[currentFileInfo.Extension].Add(currentFileInfo.Name, ((double)currentFileInfo.Length / 1024));
+                }
+                else
+                {
+                    filesInfo.Add(currentFileInfo.Extension, new Dictionary<string, double>());
+                    filesInfo[currentFileInfo.Extension].Add(currentFileInfo.Name, ((double)currentFileInfo.Length / 1024));
+                }
+            }
+            using (StreamWriter writer = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Report.txt")))
+            {
+                foreach (var item in filesInfo.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+                {
+                    writer.WriteLine(item.Key);
+                    foreach (var file in item.Value.OrderBy(x => x.Value))
+                    {
+                        writer.WriteLine($"--{file.Key} - {file.Value:f3}kb");
+                    }
+                }
+            }
+        }
+    }
+}
