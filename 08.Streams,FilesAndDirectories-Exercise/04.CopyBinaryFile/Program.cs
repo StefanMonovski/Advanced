1f8326502bb21ab1c2d25c10f9cbd6466849019f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-08 22:12:08 +0300

    Add CopyBinaryFile

---
+++ 08.Streams,FilesAndDirectories-Exercise/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream copyStream = new FileStream("../../../CopyFile.png", FileMode.Open))
+            {
+                using (FileStream pasteStream = new FileStream("../../../PasteFile.png", FileMode.Create))
+                {
+                    int byteCopy = 0;
+                    while (byteCopy != - 1)
+                    {
+                        byteCopy = copyStream.ReadByte();
+                        pasteStream.WriteByte((byte)byteCopy);
+                    }
+                }
+            }
+        }
+    }
+}
