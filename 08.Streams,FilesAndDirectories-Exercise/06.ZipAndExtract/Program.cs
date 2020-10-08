ee97f04e0f1668ff135086cf08cdbbf7b576abc9
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-08 23:08:29 +0300

    Add ZipAndExtract

---
+++ 08.Streams,FilesAndDirectories-Exercise/06.ZipAndExtract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory("../../../ZipFolder", "ZipAndExtractFile.zip");
+            ZipFile.ExtractToDirectory("ZipAndExtractFile.zip", "../../../ExtractFolder");
+        }
+    }
+}
