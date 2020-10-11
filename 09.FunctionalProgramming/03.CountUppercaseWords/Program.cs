3ec1517748ebc9eca5f86c2bbf75c0456c130c3d
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-11 20:13:47 +0300

    Add CountUppercaseWords

---
+++ 09.FunctionalProgramming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> checkUppercase = x => x[0] == x.ToUpper()[0];
+            string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(checkUppercase).ToArray();
+            foreach (string item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
