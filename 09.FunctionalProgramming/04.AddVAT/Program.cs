0a9c45f6888c768c2eebc1feef7f5acebf145541
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-11 20:20:23 +0300

    Add AddVAT

---
+++ 09.FunctionalProgramming/04.AddVAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> addVAT = x => x * 1.20;
+            double[] prices = Console.ReadLine().Split(", ").Select(x => double.Parse(x)).Select(addVAT).ToArray();
+            foreach (double item in prices)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
