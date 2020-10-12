7bb0105cb0a85b244a79715e55b1a649fd6be1ee
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-12 13:57:00 +0300

    Add KnightsOfHonor

---
+++ 10.FunctionalProgramming-Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            Action<string> printKnight = x => Console.WriteLine($"Sir {x}");
+            foreach (string item in names)
+            {
+                printKnight(item);
+            }
+        }
+    }
+}
