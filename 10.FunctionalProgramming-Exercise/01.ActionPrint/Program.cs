eb325f26b185d33c92826722206e0863fa4e12f4
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-12 13:53:07 +0300

    Add ActionPrint

---
+++ 10.FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Action<string> printWord = x => Console.WriteLine(x);
+            foreach (string item in words)
+            {
+                printWord(item);
+            }
+        }
+    }
+}
