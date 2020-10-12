510b263a72c49048e92c9e7fcf61b38f9bad533d
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-12 14:10:57 +0300

    Add CustomMinFunction

---
+++ 10.FunctionalProgramming-Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Func<int[], int> getMin = GetMin;
+            int minNumber = getMin(numbers);
+            Action<int> printMinNumber = x => Console.WriteLine(x);
+            printMinNumber(minNumber);
+        }
+
+        static int GetMin(int[] numbers)
+        {
+            int minValue = int.MaxValue;
+            foreach (int item in numbers)
+            {
+                if (item < minValue)
+                {
+                    minValue = item;
+                }
+            }
+            return minValue;
+        }
+    }
+}
