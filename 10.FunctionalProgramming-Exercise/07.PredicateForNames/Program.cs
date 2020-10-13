68b3c74ecbeeb3874c39b6e0cb73dcb29212cc23
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-13 12:28:24 +0300

    Add PredicateForNames

---
+++ 10.FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxAcceptableLength = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            Func<string[], int, List<string>> filterNamesByLength = (x, y) => x.Where(n => n.Length <= y).ToList();
+            List<string> filteredNamesByLength = filterNamesByLength(names, maxAcceptableLength);
+            Action<string> printName = x => Console.WriteLine(x);
+            foreach (string item in filteredNamesByLength)
+            {
+                printName(item);
+            }
+        }
+    }
+}
