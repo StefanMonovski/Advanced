ff83fc54bbe265014e4e9523bb1ae0fa4874301a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-13 12:15:40 +0300

    Add ReverseAndExclude

---
+++ 10.FunctionalProgramming-Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            int divideBy = int.Parse(Console.ReadLine());
+            Func<int[], int, int[]> excludeDivisibleNumbers = (x, y) => x.Where(n => n % y != 0).ToArray();
+            numbers = excludeDivisibleNumbers(numbers, divideBy);
+            Array.Reverse(numbers);
+            Action<int[]> printNumbers = x => Console.WriteLine(string.Join(" ", x));
+            printNumbers(numbers);
+        }
+    }
+}
