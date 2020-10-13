a68ab1347c287d74cec1280171bd30c1cc1297db
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-13 13:13:28 +0300

    Add CustomComparator

---
+++ 10.FunctionalProgramming-Exercise/08.CustomComparator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Array.Sort(numbers, CustomComparator);
+            Action<int[]> printNumbers = x => Console.WriteLine(string.Join(" ", x));
+            printNumbers(numbers);
+        }
+
+        static int CustomComparator(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 == 0)
+            {
+                return x.CompareTo(y);
+            }
+            if (Math.Abs(x % 2) == 1 && Math.Abs(y % 2) == 1)
+            {
+                return x.CompareTo(y);
+            }
+            if (x % 2 == 0 && Math.Abs(y % 2) == 1)
+            {
+                return -1;
+            }
+            if (Math.Abs(x % 2) == 1 && y % 2 == 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
