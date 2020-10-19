cb3cff77075592a72b984b90f1d2d6a837076da4
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-19 22:49:44 +0300

    Add OpinionPoll

---
+++ 12.DefiningClasses-Exercise/04.OpinionPoll/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+        public Person(int age)
+        : this()
+        {
+            Age = age;
+        }
+        public Person(string name, int age)
+        : this(age)
+        {
+            Name = name;
+        }
+    }
+
+    class Pollsters
+    {
+        public List<Person> Members { get; set; }
+
+        public List<Person> GetMembersOver30()
+        {
+            List<Person> membersOver30 = new List<Person>();
+            foreach (Person item in Members)
+            {
+                if (item.Age > 30)
+                {
+                    membersOver30.Add(item);
+                }
+            }
+            return membersOver30;
+        }
+    }
+}
---
+++ 12.DefiningClasses-Exercise/04.OpinionPoll/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int inputNumbers = int.Parse(Console.ReadLine());
+            Pollsters pollsters = new Pollsters() { Members = new List<Person>() };
+            for (int i = 0; i < inputNumbers; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                Person person = new Person(input[0], int.Parse(input[1]));
+                pollsters.Members.Add(person);
+            }
+            List<Person> membersOver30 = pollsters.GetMembersOver30();
+            foreach (Person item in membersOver30.OrderBy(x => x.Name))
+            {
+                Console.WriteLine(item.Name + " - " + item.Age);
+            }
+        }
+    }
+}
