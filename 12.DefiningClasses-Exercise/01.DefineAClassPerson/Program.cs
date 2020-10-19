387db6d070543c56f2a1dd896b97885266584af2
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-19 20:17:41 +0300

    Add DefineAClassPerson

---
+++ 12.DefiningClasses-Exercise/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person() { Name = "Pesho", Age = 20 };
+            Person secondPerson = new Person();
+            secondPerson.Name = "Gosho";
+            secondPerson.Age = 18;
+        }
+    }
+}
