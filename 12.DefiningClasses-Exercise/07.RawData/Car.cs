6226fbfe307d12287cf62f3f674006f213bb2e79
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-20 12:14:50 +0300

    Add RawData

---
+++ 12.DefiningClasses-Exercise/07.RawData/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.RawData
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType, double[] tiresPressure, int[] tiresAge)
+        {
+            Model = model;
+            Engine = new Engine() { EngineSpeed = engineSpeed, EnginePower = enginePower };
+            Cargo = new Cargo() { CargoWeight = cargoWeight, CargoType = cargoType };
+            Tires = new Tire[4];
+            Tires[0] = new Tire() { TirePressure = tiresPressure[0], TireAge = tiresAge[0] };
+            Tires[1] = new Tire() { TirePressure = tiresPressure[1], TireAge = tiresAge[1] };
+            Tires[2] = new Tire() { TirePressure = tiresPressure[2], TireAge = tiresAge[2] };
+            Tires[3] = new Tire() { TirePressure = tiresPressure[3], TireAge = tiresAge[3] };
+        }
+    }
+
+    class Engine
+    {
+        public int EngineSpeed { get; set; }
+        public int EnginePower { get; set; }
+
+    }
+
+    class Cargo
+    {
+        public int CargoWeight { get; set; }
+        public string CargoType { get; set; }
+
+    }
+
+    class Tire
+    {
+        public double TirePressure { get; set; }
+        public int TireAge { get; set; }
+
+    }
+}
---
+++ 12.DefiningClasses-Exercise/07.RawData/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.RawData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int numberInput = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberInput; i++)
+            {
+                string[] inputData = Console.ReadLine().Split();
+                string model = inputData[0];
+                int engineSpeed = int.Parse(inputData[1]);
+                int enginePower = int.Parse(inputData[2]);
+                int cargoWeight = int.Parse(inputData[3]);
+                string cargoType = inputData[4];
+                string[] inputPressureData = new string[] { inputData[5], inputData[7], inputData[9], inputData[11] };
+                double[] tiresPressure = inputPressureData.Select(double.Parse).ToArray();
+                string[] inputAgeData = new string[] { inputData[6], inputData[8], inputData[10], inputData[12] };
+                int[] tiresAge = inputAgeData.Select(int.Parse).ToArray();
+                Car car = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType, tiresPressure, tiresAge);
+                cars.Add(car);
+            }
+            string command = Console.ReadLine();
+            if (command == "fragile")
+            {
+                foreach (Car item in cars)
+                {
+                    if (item.Cargo.CargoType == "fragile")
+                    {
+                        foreach (Tire tire in item.Tires)
+                        {
+                            if (tire.TirePressure < 1)
+                            {
+                                Console.WriteLine(item.Model);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (command == "flamable")
+            {
+                foreach (Car item in cars)
+                {
+                    if (item.Cargo.CargoType == "flamable" && item.Engine.EnginePower > 250)
+                    {
+                        Console.WriteLine(item.Model);
+                    }
+                }
+            }
+        }
+    }
+}
