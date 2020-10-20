82cfff9b8d035dc70aadd90b8bdd7590b698aa51
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-20 11:15:09 +0300

    Add SpeedRacing

---
+++ 12.DefiningClasses-Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.SpeedRacing
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumption { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumption = fuelConsumption;
+            TravelledDistance = 0;
+        }
+
+        public void Drive(double distance)
+        {
+            if (FuelAmount - (FuelConsumption * distance) >= 0)
+            {
+                FuelAmount -= FuelConsumption * distance;
+                TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ 12.DefiningClasses-Exercise/06.SpeedRacing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SpeedRacing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int numberInput = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberInput; i++)
+            {
+                string[] inputData = Console.ReadLine().Split();
+                cars.Add(new Car(inputData[0], double.Parse(inputData[1]), double.Parse(inputData[2])));
+            }
+            string command = "";
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] commandData = command.Split();
+                cars.Find(x => x.Model == commandData[1]).Drive(double.Parse(commandData[2]));
+            }
+            foreach (Car item in cars)
+            {
+                Console.WriteLine($"{item.Model} {item.FuelAmount:f2} {item.TravelledDistance}");
+            }
+        }
+    }
+}
