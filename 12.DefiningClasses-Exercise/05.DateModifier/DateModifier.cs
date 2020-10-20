886dc789e50f6ffba3923fc199b277fe83d29323
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-20 10:47:35 +0300

    Add DateModifier

---
+++ 12.DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.DateModifier
+{
+    class DateModifier
+    {
+        public int DaysDifference { get; set; }
+
+        public int GetDaysDifference(string firstInput, string secondInput)
+        {
+            int[] firstInputData = firstInput.Split().Select(int.Parse).ToArray();
+            int[] secondInputData = secondInput.Split().Select(int.Parse).ToArray();
+            DateTime firstDate = new DateTime(firstInputData[0], firstInputData[1], firstInputData[2]);
+            DateTime secondDate = new DateTime(secondInputData[0], secondInputData[1], secondInputData[2]);
+            if (firstDate > secondDate)
+            {
+                return (int)firstDate.Subtract(secondDate).TotalDays;
+            }
+            else
+            {
+                return (int)secondDate.Subtract(firstDate).TotalDays;
+            }
+        }
+    }
+}
---
+++ 12.DefiningClasses-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _05.DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateModifier dateModifier = new DateModifier();
+            dateModifier.DaysDifference = dateModifier.GetDaysDifference(Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine(dateModifier.DaysDifference);
+        }
+    }
+}
