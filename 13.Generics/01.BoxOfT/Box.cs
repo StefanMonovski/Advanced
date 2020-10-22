8267536325a20b82b0d77dca683af21d504413ec
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 12:18:51 +0300

    Add BoxOfT

---
+++ 13.Generics/01.BoxOfT/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Stack<T> GenericBox { get; set; }
+        public int Count { get { return GenericBox.Count; } set { value = GenericBox.Count; } }
+
+        public Box()
+        {
+            GenericBox = new Stack<T>();
+        }
+
+        public void Add(T item)
+        {
+            GenericBox.Push(item);
+        }
+        public T Remove()
+        {
+            return GenericBox.Pop();
+        }
+    }
+}
