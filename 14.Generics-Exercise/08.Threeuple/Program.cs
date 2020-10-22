dd9a775aa43493d4bf1831c26f3dd856ad95f3d9
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 21:38:26 +0300

    Add Threeuple

---
+++ 14.Generics-Exercise/08.Threeuple/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _08.Threeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            Threeuple<string, string, string> firstThreeuple = new Threeuple<string, string, string>();
+            firstThreeuple.FirstItem = firstInput[0] + " " + firstInput[1];
+            firstThreeuple.SecondItem = firstInput[2];
+            if (firstInput.Length == 3)
+            {
+                firstThreeuple.ThirdItem = firstInput[3];
+            }
+            else if (firstInput.Length > 3)
+            {
+                firstThreeuple.ThirdItem = string.Join(" ", firstInput, 3, firstInput.Length - 3);
+            }
+            string[] secondInput = Console.ReadLine().Split();
+            Threeuple<string, int, bool> secondThreeuple = new Threeuple<string, int, bool>();
+            secondThreeuple.FirstItem = secondInput[0];
+            secondThreeuple.SecondItem = int.Parse(secondInput[1]);
+            if (secondInput[2] == "drunk")
+            {
+                secondThreeuple.ThirdItem = true;
+            }
+            else if (secondInput[2] == "not")
+            {
+                secondThreeuple.ThirdItem = false;
+            }
+            string[] thirdInput = Console.ReadLine().Split();
+            Threeuple<string, double, string> thirdThreeuple = new Threeuple<string, double, string>();
+            thirdThreeuple.FirstItem = thirdInput[0];
+            thirdThreeuple.SecondItem = double.Parse(thirdInput[1]);
+            thirdThreeuple.ThirdItem = thirdInput[2];
+            Console.WriteLine(firstThreeuple.ToString());
+            Console.WriteLine(secondThreeuple.ToString());
+            Console.WriteLine(thirdThreeuple.ToString());
+        }
+    }
+}
---
+++ 14.Generics-Exercise/08.Threeuple/Threeuple.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.Threeuple
+{
+    class Threeuple<T1, T2, T3>
+    {
+        public T1 FirstItem { get; set; }
+        public T2 SecondItem { get; set; }
+        public T3 ThirdItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem} -> {ThirdItem}";
+        }
+    }
+}
