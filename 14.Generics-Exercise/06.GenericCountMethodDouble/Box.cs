9888f7bb4c4968eae7621b51830e147e2a7a2615
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 16:18:09 +0300

    Add GenericCountMethodDouble

---
+++ 14.Generics-Exercise/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.GenericCountMethodDouble
+{
+    class Box<T> where T : IComparable<T>
+    {
+        public T GenericBox { get; set; }
+
+        public Box(T item)
+        {
+            GenericBox = item;
+        }
+
+        public int CompareTo(T item)
+        {
+            return GenericBox.CompareTo(item);
+        }
+
+        public override string ToString()
+        {
+            return $"{GenericBox.GetType()}: {GenericBox}";
+        }
+    }
+}
---
+++ 14.Generics-Exercise/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            List<Box<double>> boxes = new List<Box<double>>();
+            for (int i = 0; i < numberInput; i++)
+            {
+                boxes.Add(new Box<double>(double.Parse(Console.ReadLine())));
+            }
+            Box<double> compareValue = new Box<double>(double.Parse(Console.ReadLine()));
+            Console.WriteLine(Compare(boxes, compareValue));
+        }
+
+        static int Compare<T>(List<Box<T>> boxes, Box<T> compareValue) where T : IComparable<T>
+        {
+            int count = 0;
+            foreach (Box<T> item in boxes)
+            {
+                if (item.CompareTo(compareValue.GenericBox) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
