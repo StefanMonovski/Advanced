39d3aa06d8f87c03089c07f580d4b12e00071044
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 18:55:08 +0300

    Add Tuple

---
+++ 14.Generics-Exercise/07.Tuple/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            GenericTuple<string, string> firstTuple = new GenericTuple<string, string>(firstInput[0] + " " + firstInput[1], firstInput[2]);
+            string[] secondInput = Console.ReadLine().Split();
+            GenericTuple<string, int> secondTuple = new GenericTuple<string, int>(secondInput[0], int.Parse(secondInput[1]));
+            string[] thirdInput = Console.ReadLine().Split();
+            GenericTuple<int, double> thirdTuple = new GenericTuple<int, double>(int.Parse(thirdInput[0]), double.Parse(thirdInput[1]));
+            Console.WriteLine(firstTuple.ToString());
+            Console.WriteLine(secondTuple.ToString());
+            Console.WriteLine(thirdTuple.ToString());
+        }
+    }
+}
---
+++ 14.Generics-Exercise/07.Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    class GenericTuple<T1, T2>
+    {
+        public T1 FirstItem { get; set; }
+        public T2 SecondItem { get; set; }
+
+        public GenericTuple(T1 firstItem, T2 secondItem)
+        {
+            FirstItem = firstItem;
+            SecondItem = secondItem;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem}";
+        }
+    }
+}
