f0debffe915696499fdeb657475d40a9ca24a3b4
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 12:37:49 +0300

    Add GenericArrayCreator

---
+++ 13.Generics/02.GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] genericArray = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                genericArray[i] = item;
+            }
+            return genericArray;
+        }
+    }
+}
