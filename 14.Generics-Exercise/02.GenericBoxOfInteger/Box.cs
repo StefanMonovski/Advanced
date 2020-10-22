2ed9d0df2b4c6c0cd9b90b5a4d5e197fd11bf7c8
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 13:38:35 +0300

    Add GenericBoxOfInteger

---
+++ 14.Generics-Exercise/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.GenericBoxOfInteger
+{
+    class Box<T>
+    {
+        public T GenericBox { get; set; }
+
+        public Box(T item)
+        {
+            GenericBox = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{GenericBox.GetType()}: {GenericBox}";
+        }
+    }
+}
---
+++ 14.Generics-Exercise/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.GenericBoxOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberInput; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
