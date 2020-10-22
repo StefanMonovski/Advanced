6ccb3cde3b463dda7131d154ed402676e71ebebd
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 14:38:35 +0300

    Add GenericSwapMethodInteger

---
+++ 14.Generics-Exercise/04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.GenericSwapMethodInteger
+{
+    class Box<T>
+    {
+        public T GenericBox { get; set; }
+
+        public Box(T item)
+        {
+            GenericBox = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{GenericBox.GetType()}: {GenericBox}";
+        }
+    }
+}
---
+++ 14.Generics-Exercise/04.GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.GenericSwapMethodInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            List<Box<int>> boxes = new List<Box<int>>();
+            for (int i = 0; i < numberInput; i++)
+            {
+                boxes.Add(new Box<int>(int.Parse(Console.ReadLine())));
+            }
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Swap(boxes, indexes[0], indexes[1]);
+            foreach (Box<int> item in boxes)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        static void Swap<T>(List<Box<T>> boxes, int firstIndex, int secondIndex)
+        {
+            Box<T> tempBox = boxes[firstIndex];
+            boxes[firstIndex] = boxes[secondIndex];
+            boxes[secondIndex] = tempBox;
+        }
+    }
+}
