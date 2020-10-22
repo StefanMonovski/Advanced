ccceab3675fa2a4524e891e3a6beb72bd0739021
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 12:47:31 +0300

    Add GenericScale

---
+++ 13.Generics/03.GenericScale/EqualityScale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    class EqualityScale<T> 
+    {
+        public T Left { get; set; }
+        public T Right { get; set; }
+
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public bool AreEqual()
+        {
+            if (Left.Equals(Right))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 13.Generics/03.GenericScale/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> compareNumbers = new EqualityScale<int>(5, 6);
+            Console.WriteLine(compareNumbers.AreEqual());
+            EqualityScale<string> compareStrings = new EqualityScale<string>("hello", "hello");
+            Console.WriteLine(compareStrings.AreEqual());
+        }
+    }
+}
