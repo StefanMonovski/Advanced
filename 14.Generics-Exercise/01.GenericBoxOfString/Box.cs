5846e054a6bd2b7a38dc88c6462295546b06c41c
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 13:34:14 +0300

    Add GenericBoxOfString

---
+++ 14.Generics-Exercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBoxOfString
+{
+    class Box<T>
+    {
+        public T GenericBox { get; set; }
+
+        public Box(T item)
+        {
+            GenericBox = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{GenericBox.GetType()}: {GenericBox}";  
+        }
+    }
+}
---
+++ 14.Generics-Exercise/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberInput; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
