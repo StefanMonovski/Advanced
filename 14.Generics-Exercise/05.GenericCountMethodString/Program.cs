5cdf618ac5b8e808121e61bf82dc5d021c1d8bec
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-22 16:03:43 +0300

    Add GenericCountMethodString

---
+++ 14.Generics-Exercise/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.GenericCountMethodString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();
+            for (int i = 0; i < numberInput; i++)
+            {
+                boxes.Add(new Box<string>(Console.ReadLine()));
+            }
+            Box<string> compareValue = new Box<string>(Console.ReadLine());
+            Console.WriteLine(Compare(boxes, compareValue));
+        }
+
+        static int Compare<T>(List<Box<T>> boxes, Box<T> compareValue) where T: IComparable<T>
+        {
+            int count = 0;
+            foreach (Box<T> item in boxes)
+            {
+                if (item.CompareTo(compareValue.GenericBox) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
