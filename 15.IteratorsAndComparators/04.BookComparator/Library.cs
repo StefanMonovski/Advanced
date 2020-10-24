5fc96722ae202c331df158f633f9bb4b32a0f90d
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-24 14:32:55 +0300

    Add BookComparator

---
+++ 15.IteratorsAndComparators/04.BookComparator/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public List<Book> Books { get; set; }
+
+        public Library(params Book[] books)
+        {
+            Books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            Books.Sort(new BookComparator());
+            return new LibraryIterator(Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            public List<Book> Books { get; set; }
+            private int Index = -1;
+
+            public LibraryIterator(List<Book> books)
+            {
+                Books = books;
+            }
+
+            public Book Current => Books[Index];
+
+            object IEnumerator.Current => Books[Index];
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                return ++Index < Books.Count;
+            }
+
+            public void Reset()
+            {
+                Index = -1;
+            }
+        }
+    }
+}
