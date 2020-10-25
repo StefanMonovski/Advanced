0dcd372d4e77298cf7177d0cfaa44ad37edbf30d
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-25 23:49:45 +0200

    Add Classroom

---
+++ 18.Exam/03.Classroom/Classroom.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    class Classroom
+    {
+        public List<Student> Students { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return Students.Count; } set { value = Students.Count; } }
+
+        public Classroom(int capacity)
+        {
+            Students = new List<Student>();
+            Capacity = capacity;
+        }
+
+        public string RegisterStudent(Student student)
+        {
+            if (Count + 1 <= Capacity)
+            {
+                Students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+        public string DismissStudent(string firstName, string lastName)
+        {
+            if (Students.Any(x => x.FirstName == firstName && x.LastName == lastName))
+            {
+                Student removeStudent = Students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+                Students.Remove(removeStudent);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+        public string GetSubjectInfo(string subject)
+        {
+            if (!Students.Any(x => x.Subject == subject))
+            {
+                return "No students enrolled for the subject";
+            }
+            else
+            {
+                StringBuilder output = new StringBuilder();
+                output.AppendLine($"Subject: {subject}");
+                output.AppendLine("Students:");
+                foreach (Student item in Students)
+                {
+                    if (item.Subject == subject)
+                    {
+                        output.AppendLine($"{item.FirstName} {item.LastName}");
+                    }
+                }
+                return output.ToString().Trim();
+            }
+        }
+        public int GetStudentsCount()
+        {
+            return Count;
+        }
+        public Student GetStudent(string firstName, string lastName)
+        {
+            return Students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+        }
+    }
+}
---
+++ 18.Exam/03.Classroom/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassroomProject
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Classroom classroom = new Classroom(10);
+            Student student = new Student("Peter", "Parker", "Geometry");
+            Student studentTwo = new Student("Sarah", "Smith", "Algebra");
+            Student studentThree = new Student("Sam", "Winchester", "Algebra");
+            Student studentFour = new Student("Dean", "Winchester", "Music");
+            Console.WriteLine(student);
+            string register = classroom.RegisterStudent(student);
+            Console.WriteLine(register);
+            string registerTwo = classroom.RegisterStudent(studentTwo);
+            string registerThree = classroom.RegisterStudent(studentThree);
+            string registerFour = classroom.RegisterStudent(studentFour);
+            string dismissed = classroom.DismissStudent("Peter", "Parker");
+            Console.WriteLine(dismissed);
+            string dismissedTwo = classroom.DismissStudent("Ellie", "Goulding");
+            Console.WriteLine(dismissedTwo);
+            string subjectInfo = classroom.GetSubjectInfo("Algebra");
+            Console.WriteLine(subjectInfo);
+            string anotherInfo = classroom.GetSubjectInfo("Art");
+            Console.WriteLine(anotherInfo);
+            Console.WriteLine(classroom.GetStudent("Dean", "Winchester"));
+        }
+    }
+}
