51455aafe5d4b751596040dd637938f5bfe1f459
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-28 00:14:19 +0200

    Add Stack

---
+++ 16.IteratorsAndComparators-Exercise/03.Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pushInput = Console.ReadLine().Substring(5).Split(", ");
+            Stack<string> stack = new Stack<string>(pushInput.ToList());
+            string command = "";
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command.Contains("Push"))
+                {
+                    stack.Push(command.Substring(5).Split());
+                }
+                else if (command == "Pop")
+                {
+                    stack.Pop();
+                }
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (string item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+            }          
+        }
+    }
+}
---
+++ 16.IteratorsAndComparators-Exercise/03.Stack/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.Stack
+{
+    class Stack<T> : IEnumerable<T>
+    {
+        public List<T> Elements { get; set; }
+        private int index = -1;
+
+        public Stack(List<T> elements)
+        {
+            Elements = elements;
+            index += elements.Count;
+        }
+
+        public void Push(params T[] pushElements)
+        {
+            foreach (T item in pushElements)
+            {
+                Elements.Add(item);
+                index++;
+            }
+        }
+
+        public void Pop()
+        {
+            if (index < 0)
+            {
+                Console.WriteLine("No elements");
+            }
+            else
+            {
+                Elements.RemoveAt(index);
+                index--;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = index; i >= 0; i--)
+            {
+                yield return Elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
