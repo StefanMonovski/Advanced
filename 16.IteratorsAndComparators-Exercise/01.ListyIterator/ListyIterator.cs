6774449c572714b2d3559a908577b0890368a9a5
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-27 12:59:13 +0200

    Add ListyIterator

---
+++ 16.IteratorsAndComparators-Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    class ListyIterator<T>
+    {
+        public List<T> Elements { get; set; }
+        private int index = 0;
+
+        public ListyIterator(List<T> elements)
+        {
+            Elements = elements;
+        }
+
+        public bool Move()
+        {
+            if (index + 1 < Elements.Count)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (index + 1 < Elements.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Print()
+        {
+            if (Elements.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(Elements[index]);
+            }
+        }
+    }
+}
---
+++ 16.IteratorsAndComparators-Exercise/01.ListyIterator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] createInput = Console.ReadLine().Split();
+            ListyIterator<string> listyIterator = new ListyIterator<string>(createInput.Skip(1).ToList());
+            string command = "";
+            while ((command = Console.ReadLine()) != "END")
+            {
+                switch (command)
+                {
+                    case "Move": Console.WriteLine(listyIterator.Move()); break;
+                    case "HasNext": Console.WriteLine(listyIterator.HasNext()); break;
+                    case "Print": listyIterator.Print(); break;
+                }
+
+            }
+        }
+    }
+}
