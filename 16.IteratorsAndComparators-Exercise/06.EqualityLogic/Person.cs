70c23e72c845a6750e2aa691930d970bb083e813
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-28 13:25:21 +0200

    Add EqualityLogic

---
+++ 16.IteratorsAndComparators-Exercise/06.EqualityLogic/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.EqualityLogic
+{
+    class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = Age.CompareTo(other.Age);
+            }
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person personObj = obj as Person;
+            bool result = Name.Equals(personObj.Name);
+            if (result == true)
+            {
+                result = Age.Equals(personObj.Age);
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 13;
+            hash = (hash * 7) + Name.GetHashCode() + Age.GetHashCode();
+            return hash;
+        }
+    }
+}
---
+++ 16.IteratorsAndComparators-Exercise/06.EqualityLogic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> peopleSortedSet = new SortedSet<Person>();
+            HashSet<Person> peopleHashSet = new HashSet<Person>();
+            int numberInput = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberInput; i++)
+            {
+                string[] inputData = Console.ReadLine().Split();
+                peopleSortedSet.Add(new Person(inputData[0], int.Parse(inputData[1])));
+                peopleHashSet.Add(new Person(inputData[0], int.Parse(inputData[1])));
+            }
+            Console.WriteLine(peopleSortedSet.Count);
+            Console.WriteLine(peopleHashSet.Count);
+        }
+    }
+}
