9cdb14036c593a54b1596dac1883708b4af6b5fd
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-28 12:14:55 +0200

    Add Froggy

---
+++ 16.IteratorsAndComparators-Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    class Lake : IEnumerable<int>
+    {
+        public List<int> Stones { get; set; }
+
+        public Lake(List<int> stones)
+        {
+            Stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int subtractPositions = 0;
+            switch (Stones.Count % 2)
+            {
+                case 0: subtractPositions = 1; break;
+                case 1: subtractPositions = 2; break;
+            }
+            for (int i = 0; i < Stones.Count; i += 2)
+            {
+                yield return Stones[i];
+            }
+            for (int i = Stones.Count - subtractPositions; i >= 0; i -= 2)
+            {
+                yield return Stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 16.IteratorsAndComparators-Exercise/04.Froggy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            Lake lake = new Lake(input);
+            List<int> frogStonePath = new List<int>();
+            foreach (int item in lake)
+            {
+                frogStonePath.Add(item);
+            }
+            Console.WriteLine(string.Join(", ", frogStonePath));
+        }
+    }
+}
