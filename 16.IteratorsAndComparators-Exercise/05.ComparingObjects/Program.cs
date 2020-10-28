29f1ad2339f3d35d1e9f45dd5c9eea0ef7c4deac
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-28 12:39:25 +0200

    Add ComparingObjects

---
+++ 16.IteratorsAndComparators-Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string input = "";
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inputData = input.Split();
+                Person person = new Person(inputData[0], int.Parse(inputData[1]), inputData[2]);
+                people.Add(person);
+            }
+            int comparePosition = int.Parse(Console.ReadLine()) - 1;
+            int countMatches = 0;
+            int countNotMatches = 0;
+            foreach (Person item in people)
+            {
+                int compare = people[comparePosition].CompareTo(item);
+                if (compare == 0)
+                {
+                    countMatches++;
+                }
+                else
+                {
+                    countNotMatches++;
+                }
+            }
+            if (countMatches == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{countMatches} {countNotMatches} {people.Count}");
+            }
+        }
+    }
+}
