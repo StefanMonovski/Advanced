4878619debf24ee64e8cfd5fcf267ca16e6c265e
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-29 19:12:04 +0200

    Add RecursiveFactorial

---
+++ 17.BasicAlgorithms/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int factorial = RecursiveFactorial(input);
+            Console.WriteLine(factorial);
+        }
+
+        private static int RecursiveFactorial(int input)
+        {
+            if (input == 1)
+            {
+                return 1;
+            }
+            int currentFactorial = input * RecursiveFactorial(input - 1);
+            return currentFactorial;
+        }
+    }
+}
