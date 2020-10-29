1e9782d75c8ef3ffcf6f1a3abb4dc3180d1607fc
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-10-29 19:01:16 +0200

    Add RecursiveArraySum

---
+++ 17.BasicAlgorithms/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = RecursiveSum(input, 0);
+            Console.WriteLine(sum);
+        }
+
+        private static int RecursiveSum(int[] input, int index)
+        {
+            if (index == input.Length)
+            {
+                return 0;
+            }
+            int currentSum = input[index] + RecursiveSum(input, index + 1);
+            return currentSum;
+        }
+    }
+}
